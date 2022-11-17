7f9f31b4aa36d32e9b7f5308f1df100de7b6618e
yosiaLukumai <yosialukumai@gmail.com>
2022-11-17 23:46:24 +0300

    added the joke command specify the flags and logics

---
+++ cmd/joke.go
@@ -0,0 +1,135 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+type OutputInterface interface {
+	PrintOutput()
+}
+
+// struct for decoding the json
+type JokeStruct struct {
+	Category string `json:"category"`
+	Error    bool   `json:"error"`
+	Id       int    `json:"id"`
+	Joke     string `json:"joke"`
+	Safe     bool   `json:"safe"`
+}
+
+type JokeStructTwopart struct {
+	Category string `json:"category"`
+	Error    bool   `json:"error"`
+	Id       int    `json:"id"`
+	Setup    string `json:"setup"`
+	Delivery string `json:"delivery"`
+	Safe     bool   `json:"safe"`
+}
+
+// handling output err
+func (joke JokeStructTwopart) PrintOutput() {
+	if !joke.Error {
+		fmt.Printf("Id: %v,\nCategory:  %v, \nSetup: %v,\nDelivery: %v\n", joke.Id, joke.Category, joke.Setup, joke.Delivery)
+	} else {
+		fmt.Println("( ᵔ ͜ʖ ᵔ ) No such filter for the given joke")
+	}
+}
+
+func (joke JokeStruct) PrintOutput() {
+	if !joke.Error {
+		fmt.Printf("Id: %v,\nCategory:  %v,\nJoke: %v \n", joke.Id, joke.Category, joke.Joke)
+	} else {
+		fmt.Println("( ❛ ͜ʖ ❛ )No such filter for the given joke")
+	}
+}
+
+func printerOfOutput(i OutputInterface) {
+	i.PrintOutput()
+}
+
+func init() {
+	// joke.Flags().BoolP("jokeCount", "c", false, "How many joke counts can be pooled")
+	joke.Flags().IntP("amount", "a", 1, "Specify the number of the jokes to be pooled..")
+	joke.Flags().BoolP("type", "t", false, "A joke type such as twopart or single-part is false")
+	joke.Flags().StringP("category", "c", "any", "A joke category can be like programming")
+	rootCmd.AddCommand(joke)
+}
+
+var joke = &cobra.Command{
+	Use:   "joke",
+	Short: "Get a random Joke",
+	Long:  `Getting you a random joke of any category`,
+	Run: func(cmd *cobra.Command, args []string) {
+		counts, _ := cmd.Flags().GetInt("amount")
+		category, _ := cmd.Flags().GetString("category")
+		ty, _ := cmd.Flags().GetBool("type")
+		url := formUrl(category, counts, ty)
+		fmt.Printf("--> Api: %v \n", url)
+		b := GetJoke(url)
+		var jokeSingle JokeStruct
+		var jokeTwopart JokeStructTwopart
+		if ty {
+			err := json.Unmarshal(b, &jokeTwopart)
+			HandleError(err, "Failed to decode the json object")
+			printerOfOutput(jokeTwopart)
+		}
+		if !ty {
+			err := json.Unmarshal(b, &jokeSingle)
+			HandleError(err, "Failed to decode the json object")
+			printerOfOutput(jokeSingle)
+		}
+	},
+}
+
+func formUrl(category string, jokeCount int, jokeType bool) string {
+	// using  the params provided we create the url
+	// JokeApiUrl = JokeApiUrl + strconv.Itoa(jokeCount) + "/" + category
+	if category == "any" {
+		// no modificarion has been done on the category
+		return JokeApiUrl + category + createJokeType(jokeType) + createAmount(jokeCount)
+	}
+	for _, t := range JokesCategories {
+		if strings.EqualFold(t, category) {
+			return JokeApiUrl + t + createJokeType(jokeType) + createAmount(jokeCount)
+		}
+	}
+	fmt.Println("---> The flag for type joke is't correct")
+	fmt.Println("---> Default any category will be used")
+	return JokeApiUrl + "Any" + createJokeType(jokeType) + createAmount(jokeCount)
+}
+
+func createAmount(jokeCount int) string {
+	if jokeCount == 1 {
+		return ""
+	} else {
+		return "?amount=" + strconv.Itoa(jokeCount)
+	}
+}
+func createJokeType(jokeType bool) string {
+	if jokeType {
+		return "?type=twopart"
+	}
+	return "?type=single"
+}
+
+func GetJoke(url string) []byte {
+	resp, err := http.Get(url)
+	HandleError(err, "Failed to fetch the data")
+	byte, err := ioutil.ReadAll(resp.Body)
+	HandleError(err, "Failed to parse the output")
+	return byte
+}
+
+func HandleError(err error, message string) {
+	if err != nil {
+		log.Fatal(message)
+	}
+}
